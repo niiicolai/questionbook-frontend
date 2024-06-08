f87dc8513163f1c0432a08efe7784d45692eeda7
Nicolai <cwX3MfXK9YrCCc8eZk@gmail.com>
2024-06-08 20:43:02 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
 import './layout/layout.js';
 import './router.js';
+
+// Configure Content Security Policy
+const serverUrl = import.meta.env.VITE_API_HOST;
+const csp = `default-src 'self' https://cdn.tailwindcss.com; 
+             style-src 'self' https://cdn.tailwindcss.com 'unsafe-inline'; 
+             form-action 'self'; 
+             connect-src 'self' ${serverUrl};`;
+const meta = document.createElement('meta');
+meta.httpEquiv = 'Content-Security-Policy';
+meta.content = csp;
+document.head.appendChild(meta);
